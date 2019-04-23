3cfd56424df71d42b7e6df8955f09e80c20264bb
Alex Iankoulski <alex_iankoulski@yahoo.com>
2019-04-23 00:15:02 +0000

    GPU and CPU runs can now work simultaneously out of the box. Also improved output format.

--- recognize.sh
+++ recognize.sh
@@ -8,6 +8,21 @@ if [ "${1}" == "--help" ]; then
   exit 0
 fi
 
+echo ""
+echo "==========================================="
+echo "Processor: ${PROCESSOR_TYPE}"
+echo "Face recognition start time:"
+date
+STARTTIME=$(date +%s)
+echo ""
+
 ./run.sh face_recognition /wd/data/known_people /wd/data/images/$1 --show-distance True --tolerance 0.586 --cpus -1 | grep -v None
 
+echo ""
+echo "Face recognition end time: "
+date
+ENDTIME=$(date +%s)
+echo "Total elapsed: $(($ENDTIME - $STARTTIME)) seconds"
+echo "==========================================="
+echo ""
 
--- run.sh
+++ run.sh
@@ -8,5 +8,6 @@ else
 	MODE="-it --rm"
 fi 
 
+echo "docker container run ${RUN_OPTS} ${CONTAINER_NAME} ${MODE} ${NETWORK} ${PORT_MAP} ${VOL_MAP} ${REGISTRY}${IMAGE}${TAG} $@"
 docker container run ${RUN_OPTS} ${CONTAINER_NAME} ${MODE} ${NETWORK} ${PORT_MAP} ${VOL_MAP} ${REGISTRY}${IMAGE}${TAG} $@
 
